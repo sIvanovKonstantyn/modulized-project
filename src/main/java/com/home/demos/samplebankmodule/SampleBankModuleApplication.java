5607698b895d06ceace0e8dde809701d9e84f86c
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2022-06-25 15:58:51 +0200

    4 VERSION: java 11 + maven + Default spring boot (2.7.1) + spring data jdbc excluded autoconfigurable

--- src/main/java/com/home/demos/samplebankmodule/SampleBankModuleApplication.java
+++ src/main/java/com/home/demos/samplebankmodule/SampleBankModuleApplication.java
@@ -1,13 +1,17 @@
 package com.home.demos.samplebankmodule;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableWebMvc
 public class SampleBankModuleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleBankModuleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SampleBankModuleApplication.class, args);
+    }
 
 }
--- src/main/java/com/home/demos/samplebankmodule/config/SampleBankModuleConfiguration.java
+++ src/main/java/com/home/demos/samplebankmodule/config/SampleBankModuleConfiguration.java
@@ -1,41 +1,52 @@
 package com.home.demos.samplebankmodule.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.TransactionManager;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SampleBankModuleConfiguration {
 
-    @Value("${db.driver}")
-    private String driver;
-    @Value("${db.url}")
-    private String url;
-    @Value("${db.user}")
-    private String user;
-    @Value("${db.password}")
-    private String password;
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ApplicationContext context;
 
     @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(driver);
-        dataSource.setUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.user"));
+        dataSource.setPassword(environment.getProperty("db.password"));
+
+        System.out.println("BEANS:\n"
+                + String.join("\n", context.getBeanDefinitionNames())
+                + "...BEANS\n"
+        );
 
         return dataSource;
     }
